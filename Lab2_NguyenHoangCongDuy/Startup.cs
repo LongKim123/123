36f5389fcb5941a5decba7a02e38a766b8604556
kimlongxutede110499@outlook.com <kimlongxutede110499@outlook.com>
2020-06-05 08:06:38 +0700

    CHao

---
+++ Lab2_NguyenHoangCongDuy/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Lab2_NguyenHoangCongDuy.Startup))]
+namespace Lab2_NguyenHoangCongDuy
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
